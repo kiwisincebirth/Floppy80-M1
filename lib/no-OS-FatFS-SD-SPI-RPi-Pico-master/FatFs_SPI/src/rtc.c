dbd8214fe6bbad266de93d951bc7a3aa7f93739d
MichaelM4 <45742306+MichaelM4@users.noreply.github.com>
2024-10-27 13:34:13 -0400

    Supports easier switch from pico to pico 2.

--- lib/no-OS-FatFS-SD-SPI-RPi-Pico-master/FatFs_SPI/src/rtc.c
+++ lib/no-OS-FatFS-SD-SPI-RPi-Pico-master/FatFs_SPI/src/rtc.c
@@ -24,15 +24,17 @@ specific language governing permissions and limitations under the License.
 //
 #include "rtc.h"
 
-// typedef struct {
-//     int16_t year;    ///< 0..4095
-//     int8_t month;    ///< 1..12, 1 is January
-//     int8_t day;      ///< 1..28,29,30,31 depending on month
-//     int8_t dotw;     ///< 0..6, 0 is Sunday
-//     int8_t hour;     ///< 0..23
-//     int8_t min;      ///< 0..59
-//     int8_t sec;      ///< 0..59
-// } datetime_t;
+#ifndef datetime_t
+typedef struct {
+    int16_t year;    ///< 0..4095
+    int8_t month;    ///< 1..12, 1 is January
+    int8_t day;      ///< 1..28,29,30,31 depending on month
+    int8_t dotw;     ///< 0..6, 0 is Sunday
+    int8_t hour;     ///< 0..23
+    int8_t min;      ///< 0..59
+    int8_t sec;      ///< 0..59
+} datetime_t;
+#endif
 
 static time_t epochtime;
 
